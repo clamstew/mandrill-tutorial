298c7bd4517d8a72023b4e9e3a0ab45183669986
Clay Stewart <claytonmstewart@gmail.com>
2013-09-30 13:46:30 -0500

    Final commit for the first part of the tutorial

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'sinatra-contrib'
+gem 'thin'
+gem 'mandrill-api'
--- mandrill-tutorial.rb
+++ mandrill-tutorial.rb
@@ -2,7 +2,17 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'thin'
+require 'mandrill'
+
+require_relative 'lib/email'
 
 get '/' do
-  'Welcome to your Penpal app!'
+  'Welcome to your Mail Sender app!'
+end
+
+get '/send/:email' do
+  m = MyEmailer.new
+  m.send(params[:email])
+  "Email Sent"
 end
